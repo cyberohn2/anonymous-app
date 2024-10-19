fb3764e5d84da68bef1962b0821b3f81486a7d92
Cyberohn <95048462+Cyberohn@users.noreply.github.com>
2024-10-19 15:21:40 +0100

    Update MessageItem.jsx

--- src/components/MessageItem.jsx
+++ src/components/MessageItem.jsx
@@ -35,7 +35,7 @@ const MessageItem = ({ message }) => {
   };
 
   return (
-    <div className="flex flex-col items-center mb-4">
+    <div className="flex flex-col items-center pb-4 border-b border-gray-500">
       {/* Message container with gradient background */}
       <div
         ref={messageRef}
